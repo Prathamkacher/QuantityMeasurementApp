0de7cc598f2bc0dd012ba78a990469efa090f15c
Prathamkacher <pratham9340@gmail.com>
2026-02-20 16:28:34 +0530

    [Pratham]: added UC1 feetEquality feature

---
+++ src/main/java/com/quantitymeasurement/QuantityMeasurementApp.java
@@ -0,0 +1,52 @@
+package com.quantitymeasurement;
+
+import java.util.Scanner;
+
+public class QuantityMeasurementApp {
+	public static class Feet {
+
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        public double getValue() {
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+
+            Feet other = (Feet) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+
+    public static void main(String[] args) {
+    	
+        Scanner sc = new Scanner(System.in);
+        
+        double inputOne = sc.nextDouble();       
+        double inputTwo = sc.nextDouble();
+        
+        Feet feet1 = new Feet(inputOne);
+        Feet feet2 = new Feet(inputTwo);
+
+        System.out.println("Equal (" + feet1.equals(feet2) + ")");
+        sc.close();
+    }
+
+}
