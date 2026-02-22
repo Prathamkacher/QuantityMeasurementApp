cd99deb0e4921c7f896b87c6710563ea307e3cac
Prathamkacher <pratham9340@gmail.com>
2026-02-22 23:41:03 +0530

    [Pratham]: added feature 2 Inch Equality

--- src/main/java/com/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/quantitymeasurement/QuantityMeasurementApp.java
@@ -3,6 +3,8 @@ package com.quantitymeasurement;
 import java.util.Scanner;
 
 public class QuantityMeasurementApp {
+	
+	//----------Feet class-----------
 	public static class Feet {
 
         private final double value;
@@ -34,19 +36,51 @@ public class QuantityMeasurementApp {
             return Double.hashCode(value);
         }
     }
+	
+	//----------Inch class-----------
+	public static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+	
+	public static boolean compareFeet(double value1, double value2) {
+        Feet f1 = new Feet(value1);
+        Feet f2 = new Feet(value2);
+        return f1.equals(f2);
+    }
+
+    public static boolean compareInches(double value1, double value2) {
+        Inches i1 = new Inches(value1);
+        Inches i2 = new Inches(value2);
+        return i1.equals(i2);
+    }
 
     public static void main(String[] args) {
     	
-        Scanner sc = new Scanner(System.in);
-        
-        double inputOne = sc.nextDouble();       
-        double inputTwo = sc.nextDouble();
-        
-        Feet feet1 = new Feet(inputOne);
-        Feet feet2 = new Feet(inputTwo);
+    	System.out.println("Input: 1.0 inch and 1.0 inch");
+        System.out.println("Equal (" + compareInches(1.0, 1.0) + ")");
 
-        System.out.println("Equal (" + feet1.equals(feet2) + ")");
-        sc.close();
+        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Equal (" + compareFeet(1.0, 1.0) + ")");
     }
-
 }
