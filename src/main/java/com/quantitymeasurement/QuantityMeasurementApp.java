ea67e95a09cc3f876af7d27a93e82192eeb385a5
Prathamkacher <pratham9340@gmail.com>
2026-02-24 10:20:55 +0530

    [Pratham]: added feature/UC12 - Implimented Subtraction and Division Operations on Quantity Measurements

--- src/main/java/com/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/quantitymeasurement/QuantityMeasurementApp.java
@@ -4,21 +4,21 @@ public class QuantityMeasurementApp {
 
 	public static void main(String[] args) {
 
-		Quantity<LengthUnit> length1 = new Quantity<>(10.0, LengthUnit.FEET);
-		Quantity<LengthUnit> length2 = new Quantity<>(6.0, LengthUnit.INCHES);
+        Quantity<LengthUnit> length1 = new Quantity<>(10.0, LengthUnit.FEET);
+        Quantity<LengthUnit> length2 = new Quantity<>(6.0, LengthUnit.INCHES);
 
-		System.out.println("Subtract Length: " + length1.subtract(length2));
+        System.out.println("Subtract Length: " + length1.subtract(length2));
 
-		System.out.println("Subtract Explicit (Inches): " + length1.subtract(length2, LengthUnit.INCHES));
+        System.out.println("Subtract Explicit (Inches): " + length1.subtract(length2, LengthUnit.INCHES));
 
-		Quantity<WeightUnit> weight1 = new Quantity<>(10.0, WeightUnit.KILOGRAM);
-		Quantity<WeightUnit> weight2 = new Quantity<>(5.0, WeightUnit.KILOGRAM);
+        Quantity<WeightUnit> weight1 = new Quantity<>(10.0, WeightUnit.KILOGRAM);
+        Quantity<WeightUnit> weight2 = new Quantity<>(5.0, WeightUnit.KILOGRAM);
 
-		System.out.println("Division Weight: " + weight1.divide(weight2));
+        System.out.println("Division Weight: " + weight1.divide(weight2));
 
-		Quantity<VolumeUnit> volume1 = new Quantity<>(5.0, VolumeUnit.LITRE);
-		Quantity<VolumeUnit> volume2 = new Quantity<>(10.0, VolumeUnit.LITRE);
+        Quantity<VolumeUnit> volume1 = new Quantity<>(5.0, VolumeUnit.LITRE);
+        Quantity<VolumeUnit> volume2 = new Quantity<>(10.0, VolumeUnit.LITRE);
 
-		System.out.println("Division Volume: " + volume1.divide(volume2));
-	}
+        System.out.println("Division Volume: " + volume1.divide(volume2));
+    }
 }
--- src/main/java/com/quantitymeasurement/WeightUnit.java
+++ src/main/java/com/quantitymeasurement/WeightUnit.java
@@ -1,30 +1,32 @@
 package com.quantitymeasurement;
 
-public enum WeightUnit implements IMeasurable {
+public enum WeightUnit implements IMeasurable{
 
-	KILOGRAM(1.0), GRAM(0.001), POUND(0.453592);
+    KILOGRAM(1.0),
+    GRAM(0.001),
+    POUND(0.453592);
 
-	private final double conversionFactor;
+    private final double conversionFactor;
 
-	WeightUnit(double conversionFactor) {
-		this.conversionFactor = conversionFactor;
-	}
+    WeightUnit(double conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
 
-	public double getConversionFactor() {
-		return conversionFactor;
-	}
+    public double getConversionFactor() {
+        return conversionFactor;
+    }
 
-	public double convertToBaseUnit(double value) {
-		double result = value * conversionFactor;
-		return Math.round(result * 100.0) / 100.0;
-	}
+    public double convertToBaseUnit(double value) {
+        double result = value * conversionFactor;
+        return Math.round(result * 100.0) / 100.0;
+    }
 
-	public double convertFromBaseUnit(double baseValue) {
-		double result = baseValue / conversionFactor;
-		return Math.round(result * 100.0) / 100.0;
-	}
-
-	public String getUnitName() {
-		return this.name();
-	}
+    public double convertFromBaseUnit(double baseValue) {
+        double result = baseValue / conversionFactor;
+        return Math.round(result * 100.0) / 100.0;
+    }
+    
+    public String getUnitName() {
+    	return this.name();
+    }
 }
