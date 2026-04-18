38144175d9a380e26b7310cc241b757d08c9aeee
Prathamkacher <pratham9340@gmail.com>
2026-04-18 10:46:40 +0530

    [Pratham]: Removed unnecessary folder from UC21

--- src/main/java/com/app/quantitymeasurement/repository/HistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.quantitymeasurement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.app.quantitymeasurement.entity.History;
-
-public interface HistoryRepository extends JpaRepository<History, Long> {
-}
